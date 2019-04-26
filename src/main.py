153923fb5290a1c0ea7016996ee562cb9b2a4887
danilyanich <danilyanich@gmail.com>
2019-04-26 13:24:00 +0300

    Use parsed document data

--- src/main.py
+++ src/main.py
@@ -1,40 +1,60 @@
-from sklearn.datasets import fetch_20newsgroups_vectorized as dataset
-
 import methods as mt
 import utils as ut
 import matplotlib.pyplot as plt
-# from scipy.sparse import csr_matrix
+import numpy as np
+import parser as ps
+import re
+import sys
 
-RANK_K = 4
-EPS = 10e-3
+
+RANK_K = 1
+EPS = 10e-5
+
+np.set_printoptions(
+  precision=3,
+  suppress=True,
+  formatter={'float': '{: 0.3f}'.format}
+)
+
+
+def __do_methods(methods):
+  best_result = None
+  error = 10e10
+
+  for name, generator in methods.items():
+    print('# {}'.format(name))
+    result, errors = ut.iterate(generator, A, EPS)
+
+    plt.plot(errors, label=name)
+    print()
+
+    if error > errors[-1]:
+      best_result = result
+      error = errors[-1]
+
+  plt.legend()
+  plt.show()
+
+  return best_result
 
 
 if __name__ == '__main__':
-  A = dataset().data# [:5000, :10000]
+  text = re.sub('[\n" ]+', ' ', sys.stdin.read())
+  sentences = [re.sub('^ +', '', s) for s in re.split('[!.?]+', text)]
 
-  # A = csr_matrix([
-  #   [5, 1, 3, 5, 2],
-  #   [0, 0, 0, 2, 5],
-  #   [4, 0, 3, 0, 1],
-  #   [1, 5, 5, 3, 3]
-  # ])
-
+  A = ps.get_weighted_term_document_matrix(sentences)
   W, H = ut.initialize_random(*A.shape, RANK_K)
 
-  methods = {
+  W, H = __do_methods({
     'alternating_least_squares_solve': mt.alternating_least_squares_solve(A, W, H),
     'alternating_least_squares_nnls': mt.alternating_least_squares_nnls(A, W, H),
     'alternating_least_squares_lstsq': mt.alternating_least_squares_lstsq(A, W, H),
     'multiplicative_update_rule': mt.multiplicative_update_rule(A, W, H)
-  }
+  })
 
-  for name, generator in methods.items():
-    print('# {}'.format(name))
-    _, errors = ut.iterate(generator, A, EPS)
-    print()
+  data = sorted(zip(W.T[0], sentences), key=lambda t: t[0], reverse=True)[:5]
 
-    plt.plot(errors, label=name)
-
-  plt.legend()
-  plt.show()
+  for d in data:
+    score, sentence = d
+    print('{:.3f} {}'.format(score, sentence))
   pass
