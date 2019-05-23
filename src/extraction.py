b1c6e7bcbf9a8c6e9290c6fc9a2bcd218cadeb0d
danilyanich <danilyanich@gmail.com>
2019-05-23 14:07:58 +0300

    Add saliency and qr key factors extraction

--- src/extraction.py
+++ src/extraction.py
@@ -2,15 +2,29 @@ import scipy.linalg as lg
 import numpy as np
 
 
-def get_key_sentence_numbers(W, H):
-  _, rank = W.shape
-  _, _, permutations = lg.qr(H, pivoting=True)
+def get_saliency_key_factors(W, H):
+  '''Extract key factors based on saliency score'''
 
-  return permutations[:rank]
+  n_terms, _ = W.shape
+  _, n_sentences = H.shape
+
+  _, column_permutations = zip(*sorted(
+    zip(H.T, range(n_sentences)),
+    key=lambda t: -sum(t[0])
+  ))
+
+  _, row_permutations = zip(*sorted(
+    zip(W, range(n_terms)),
+    key=lambda t: -sum(t[0])
+  ))
+
+  return column_permutations, row_permutations
 
 
-def get_key_word_numbers(W, H):
-  _, rank = W.shape
-  _, _, permutations = lg.qr(W.T, pivoting=True)
+def get_qr_key_factors(W, H):
+  '''Extract key factors based on QR decomposition with column pivoting'''
 
-  return permutations[:rank]
+  _, _, column_permutations = lg.qr(H, pivoting=True)
+  _, _, row_permutations = lg.qr(W.T, pivoting=True)
+
+  return column_permutations, row_permutations
