787257b1691d919c15d7f043b9c0c6029fdf2aa4
danilyanich <danilyanich@gmail.com>
2019-06-07 23:24:16 +0300

    Add script for factorization

---
+++ scripts/factorize-samples.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -euo pipefail
+
+FILES=$(ls samples/*.parsed)
+METHODS=(MU ALS_NORM ALS_NNLS ALS_LSQR)
+PRECISION='10e-2'
+RANK='5'
+
+for file in $FILES; do
+  for method in "${METHODS[@]}"; do
+    echo \r $file $method
+    bin/factorize $file --eps $PRECISION --rank $RANK --method $method
+  done;
+done;
